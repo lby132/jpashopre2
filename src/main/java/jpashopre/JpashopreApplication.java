e6b230e8668b26ff6c83b8fc2568272881351436
lby132 <quddbscjswow@naver.com>
2023-03-19 00:58:22 +0900

    Initial commit

---
+++ src/main/java/jpashopre/JpashopreApplication.java
@@ -0,0 +1,13 @@
+package jpashopre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpashopreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jpashopre/JpashopreApplicationTests.java
@@ -0,0 +1,13 @@
+package jpashopre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpashopreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
