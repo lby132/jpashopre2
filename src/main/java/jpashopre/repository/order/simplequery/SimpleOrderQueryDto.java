4bbd01cda180a8bddb31e16f84e00762551c5ad9
pro <quddbscjswow@naver.com>
2023-03-25 22:00:17 +0900

    basic

---
+++ src/main/java/jpashopre/repository/order/simplequery/SimpleOrderQueryDto.java
@@ -0,0 +1,24 @@
+package jpashopre.repository;
+
+import jpashopre.domain.Address;
+import jpashopre.domain.OrderStatus;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class SimpleOrderQueryDto {
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate;
+    private OrderStatus orderStatus;
+    private Address address;
+
+    public SimpleOrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+    }
+}
